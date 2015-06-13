0a84634500770d00ec78a68163999e3bbe659666
Jorge Sanchez <jorjatorz@gmail.com>
2015-06-13 23:23:31 +0200

    Added some functionalities to FTransformationObject

--- LastStand/FTransformationObject.cpp
+++ LastStand/FTransformationObject.cpp
@@ -4,13 +4,65 @@ FTransformationObject::FTransformationObject(std::string name, FTransformationOb
 	:_name(name),
 	_parentTransformationObject(parent),
 	_localPosition(0.0),
-	_worldPosition(0.0),
-	_rotationValue(),
-	_scaleValue(1.0)
+	_localRotationValue(),
+	_localScaleValue(1.0)
 {
+	//If a parent exist register to it
+	if (_parentTransformationObject)
+	{
+		_parentTransformationObject->addChildrenTranformationObject(this);
+	}
+	getWorldTransformationsFromParent();
 }
 
 
 FTransformationObject::~FTransformationObject()
 {
+}
+
+void FTransformationObject::setLocalTransformation(const Vector3& deltaPos, const Quaternion& deltaRot, const Vector3& deltaScale)
+{
+	_localPosition += deltaPos;
+	_localRotationValue = deltaRot * _localRotationValue;
+	_localScaleValue = _localScaleValue * deltaScale;
+
+	//Update _world transformations
+	getWorldTransformationsFromParent();
+	updateChildrensTransformationObjects();
+}
+
+
+void FTransformationObject::updateChildrensTransformationObjects()
+{
+	for (auto it: _childrenTransformationObjectsList)
+	{
+		it.second->getWorldTransformationsFromParent();
+	}
+}
+
+void FTransformationObject::getWorldTransformationsFromParent()
+{
+	if (_parentTransformationObject)
+	{
+		_worldPosition = _parentTransformationObject->_worldPosition;
+		_worldRotationValue = _parentTransformationObject->_worldRotationValue;
+		_worldScaleValue = _parentTransformationObject->_worldScaleValue;
+	}
+	else
+	{
+		_worldPosition = _localPosition;
+		_worldRotationValue = _localRotationValue;
+		_worldScaleValue = _localScaleValue;
+	}
+}
+
+void FTransformationObject::addChildrenTranformationObject(FTransformationObject* children)
+{
+	if (children)
+		_childrenTransformationObjectsList.insert(std::make_pair(children->_name, children));
+}
+
+void FTransformationObject::removeChildrenTransformationObject(std::string name)
+{
+	_childrenTransformationObjectsList.erase(name);
 }
--- LastStand/FTransformationObject.h
+++ LastStand/FTransformationObject.h
@@ -11,8 +11,40 @@
 class FTransformationObject
 {
 public:
+	//Updates world positions and REGISTER to parent (if exists)
 	FTransformationObject(std::string name, FTransformationObject* parent = NULL);
 	~FTransformationObject();
+
+	void addChildrenTranformationObject(FTransformationObject* children);
+	void removeChildrenTransformationObject(std::string name);
+
+	//Modify the local transformation of the object. Updates the world transformation and notify to children.
+	void setLocalTransformation(const Vector3& deltaPos, const Quaternion& deltaRot, const Vector3& deltaScale);
+
+	Vector3 getLocalPosition()
+	{
+		return _localPosition;
+	}
+	Vector3 getWorldPosition()
+	{
+		return _worldPosition;
+	}
+	Quaternion getLocalOrientation()
+	{
+		return _localRotationValue;
+	}
+	Quaternion getWolrdOrientation()
+	{
+		return _worldRotationValue;
+	}
+	Vector3 getLocalScale()
+	{
+		return _localScaleValue;
+	}
+	Vector3 getWorldScale()
+	{
+		return _worldScaleValue;
+	}
 
 private:
 	//Name of the FTransformationObject
@@ -20,12 +52,16 @@ private:
 	//Parent of the FTransformationObject. Null if no parent
 	FTransformationObject* _parentTransformationObject;
 	//Children map
-	std::unordered_map<std::string, FTransformationObject> _childrenTransformationObjectsList;
+	std::unordered_map<std::string, FTransformationObject*> _childrenTransformationObjectsList;
+	void updateChildrensTransformationObjects(); //Updates all the childrens with the new transformations
+	void getWorldTransformationsFromParent(); //Get the world transformation from the parent. If no parent then worldTrans = localTrans
 
 	//Transformation attributes
 	Vector3 _localPosition; //It's own position
 	Vector3 _worldPosition; //Total position (localposition + parentposition)
-	Quaternion _rotationValue;
-	Vector3 _scaleValue;
+	Quaternion _localRotationValue;
+	Quaternion _worldRotationValue;
+	Vector3 _localScaleValue;
+	Vector3 _worldScaleValue;
 };
 
