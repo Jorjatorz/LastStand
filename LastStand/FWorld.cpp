1187dfd3486d4a10b5d3d6ab33e8b79f4440634d
Jorge Sanchez <jorjatorz@gmail.com>
2015-06-14 20:43:52 +0200

    Added the ability to add entities to the FWorld

--- LastStand/FWorld.cpp
+++ LastStand/FWorld.cpp
@@ -1,5 +1,7 @@
 #include "FWorld.h"
 
+#include "FLog.h"
+#include "Entity.h"
 
 FWorld::FWorld()
 {
@@ -8,4 +10,38 @@ FWorld::FWorld()
 
 FWorld::~FWorld()
 {
+	for (auto it : _entityInWorldList)
+	{
+		delete it.second;
+	}
+	_entityInWorldList.clear();
+}
+
+Entity* FWorld::addEntityToWorld(std::string name)
+{
+	//If already exists
+	if (_entityInWorldList.find(name) != _entityInWorldList.end())
+	{
+		FLog(FLog::ERROR, "Entity already exists in the world: %s", name.c_str());
+		assert(0);
+	}
+
+	Entity* ent = new Entity(name);
+	_entityInWorldList.insert(std::make_pair(name, ent));
+
+	return ent;
+}
+
+void FWorld::deleteEntityFromWorld(std::string name)
+{
+	//If doesnt exists
+	auto it = _entityInWorldList.find(name);
+	if (it == _entityInWorldList.end())
+	{
+		FLog(FLog::ERROR, "Entity doesn't exists in the world, can't delete it: %s", name.c_str());
+		assert(0);
+	}
+
+	delete it->second;
+	_entityInWorldList.erase(it);
 }
--- LastStand/FWorld.h
+++ LastStand/FWorld.h
@@ -1,10 +1,21 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+
+class Entity;
+
 //Class that will manage the world. Will contain all the entities, cameras, lights, etc
 class FWorld
 {
 public:
 	FWorld();
 	~FWorld();
+
+	Entity* addEntityToWorld(std::string name);
+	void deleteEntityFromWorld(std::string name);
+
+private:
+	std::unordered_map<std::string, Entity*> _entityInWorldList;
 };
 
