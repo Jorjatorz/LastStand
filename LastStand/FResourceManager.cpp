262b4159affb3221ba7bcd279dd8a4c2e299bfca
Jorge Sanchez <jorjatorz@gmail.com>
2015-06-14 23:22:36 +0200

    FResourceManager implemented

--- LastStand/FResourceManager.cpp
+++ LastStand/FResourceManager.cpp
@@ -1,5 +1,12 @@
 #include "FResourceManager.h"
 
+#include "Mesh.h"
+#include "Shader.h"
+#include "Texture.h"
+
+#include "FLog.h"
+
+#include <assert.h>
 
 FResourceManager::FResourceManager()
 {
@@ -8,4 +15,206 @@ FResourceManager::FResourceManager()
 
 FResourceManager::~FResourceManager()
 {
+	//Delete all shaders
+	for( auto it : _shaderMap)
+	{
+		delete it.second;
+	}
+	_shaderMap.clear();
+	//Delete all textures
+	for (auto it : _textureMap)
+	{
+		delete it.second;
+	}
+	_textureMap.clear();
+	//Delete all mshes
+	for (auto it : _meshMap)
+	{
+		delete it.second;
+	}
+	_meshMap.clear();
+}
+
+Shader* FResourceManager::loadShaderIntoMemoryFromDisk(std::string shaderName)
+{
+	FLog(FLog::INFO, "Loading Shader " + shaderName);
+
+	auto it = _shaderMap.find(shaderName);
+	if (it != _shaderMap.end())
+	{
+		FLog(FLog::ERROR, "Can't load the shader into memory, it already exist: " + shaderName);
+		assert(0);
+	}
+
+	//Create a new shader with a name(to identify it in the log)
+	Shader* newShader = new Shader(shaderName);
+	//Load the shader from a file
+	if (newShader->loadFromDisk("Data\\Shaders\\" + shaderName))
+	{
+		FLog(FLog::INFO, shaderName + " shader loaded.");
+		_shaderMap.emplace(std::make_pair(shaderName, newShader));
+	}
+	else
+	{
+		FLog(FLog::ERROR, "Couldn't load Shader: Data\\Shaders\\" + shaderName);
+		delete newShader;
+	}
+
+	return newShader;
+
+}
+void FResourceManager::deleteShaderFromMemory(std::string shaderName)
+{
+	auto it = _shaderMap.find(shaderName);
+	if (it == _shaderMap.end())
+	{
+		FLog(FLog::ERROR, "Can't delete the shader from memory, it doesnt exist: " + shaderName);
+		assert(0);
+	}
+
+	delete it->second;
+	_shaderMap.erase(it);
+}
+Shader* FResourceManager::getShaderInMemory(std::string shaderName)
+{
+	auto it = _shaderMap.find(shaderName);
+	if (it == _shaderMap.end())
+	{
+		FLog(FLog::ERROR, "Can't return the shader in memory, it doesnt exist: " + shaderName);
+		assert(0);
+	}
+
+	return it->second;
+}
+
+void FResourceManager::reloadAllShadersFromDisk()
+{
+	FLog(FLog::INFO, "Reloading all the shaders from its file");
+	for(auto it : _shaderMap)
+	{
+		it.second->loadFromDisk("Data\\Shaders\\" + it.first);
+	}
+}
+
+Texture* FResourceManager::loadTextureIntoMemoryFromDisk(std::string textureName)
+{
+	FLog(FLog::INFO, "Loading Texture " + textureName);
+
+	auto it = _textureMap.find(textureName);
+	if (it != _textureMap.end())
+	{
+		FLog(FLog::ERROR, "Can't load the texture into memory, it already exists: " + textureName);
+		assert(0);
+	}
+
+	Texture* newTexture = new Texture();
+	//Load the texture from a file, by default with mipmap and RGBA format
+	if (newTexture->loadFromDisk("Data\\Textures\\" + textureName, true, GL_RGBA))
+	{
+		FLog(FLog::INFO, textureName + " texture loaded.");
+		_textureMap.emplace(std::make_pair(textureName, newTexture));
+	}
+	else
+	{
+		FLog(FLog::ERROR, "Couldn't load texture: Data\\Textures\\" + textureName);
+		delete newTexture;
+	}
+
+	return newTexture;
+}
+
+Texture* FResourceManager::createNewTextureInMemory(std::string textureName, unsigned int width, unsigned int height, GLint format, bool mipmap)
+{
+	auto it = _textureMap.find(textureName);
+	if (it != _textureMap.end())
+	{
+		FLog(FLog::ERROR, "Can't create the texture into memory, it already exists: " + textureName);
+		assert(0);
+	}
+
+	Texture* newTexture = new Texture();
+	//Generate the new texture without pixels
+	newTexture->generateRawTexture(width, height, format, mipmap, NULL);
+
+	FLog(FLog::INFO, "Texture Created: " + textureName);
+
+	return newTexture;
+}
+
+void FResourceManager::deleteTextureFromMemory(std::string textureName)
+{
+	auto it = _textureMap.find(textureName);
+	if (it == _textureMap.end())
+	{
+		FLog(FLog::ERROR, "Can't delete the texture from memory, it doesnt exist: " + textureName);
+		assert(0);
+	}
+
+	delete it->second;
+	_textureMap.erase(it);
+}
+
+Texture* FResourceManager::getTextureInMemory(std::string textureName)
+{
+	auto it = _textureMap.find(textureName);
+	if (it == _textureMap.end())
+	{
+		FLog(FLog::ERROR, "Can't return the texture into memory, it doesnt exist: " + textureName);
+		assert(0);
+	}
+
+	return it->second;
+}
+
+//Load a mesht into the memory
+Mesh* FResourceManager::loadMeshIntoMemoryFromDisk(std::string meshName)
+{
+	FLog(FLog::INFO, "Loading Mesh " + meshName);
+
+	auto it = _meshMap.find(meshName);
+	if (it != _meshMap.end())
+	{
+		FLog(FLog::ERROR, "Mesh already into memory, can't load it " + meshName);
+		assert(0);
+	}
+
+	Mesh* newMesh = new Mesh();
+	//Load the mesh from a file
+	if (newMesh->loadMesh("Data\\Meshes\\" + meshName))
+	{
+		FLog(FLog::INFO, meshName + " mesh loaded.");
+		_meshMap.emplace(std::make_pair(meshName, newMesh));
+	}
+	else
+	{
+		FLog(FLog::ERROR, "Couldn't load mesh: Data\\Meshes\\" + meshName);
+		delete newMesh;
+	}
+
+	return newMesh;
+}
+//Delete a mesh from memory
+void FResourceManager::deleteMeshFromMemory(std::string meshName)
+{
+	auto it = _meshMap.find(meshName);
+	if (it == _meshMap.end())
+	{
+		FLog(FLog::ERROR, "Can't delete the mesh from memory, it doesnt exist: " + meshName);
+		assert(0);
+	}
+
+	delete it->second;
+	_meshMap.erase(it);
+}
+//Return the mesh stored in meory
+Mesh* FResourceManager::getMeshInMemory(std::string meshName)
+{
+	auto it = _meshMap.find(meshName);
+	if (it == _meshMap.end())
+	{
+		FLog(FLog::ERROR, "Can't return the mesh in memory, it doesnt exist: " + meshName);
+		assert(0);
+	}
+
+	return it->second;
 }
--- LastStand/FResourceManager.h
+++ LastStand/FResourceManager.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <unordered_map>
+
+#include <gl/glew.h>
+
+class Mesh;
+class Shader;
+class Texture;
 
 //Class that manage all external resources that are added to the game.
 class FResourceManager
@@ -7,5 +14,38 @@ class FResourceManager
 public:
 	FResourceManager();
 	~FResourceManager();
+
+	//Load a new shader into the memory
+	Shader* loadShaderIntoMemoryFromDisk(std::string shaderName);
+	//Delete the shader stored in memory
+	void deleteShaderFromMemory(std::string shaderName);
+	//Return the shader stored in memory
+	Shader* getShaderInMemory(std::string shaderName);
+	//Reload all the shaders from his file
+	void reloadAllShadersFromDisk();
+
+	//Load a new Texture into the memory
+	Texture* loadTextureIntoMemoryFromDisk(std::string textureName);
+	//Creates a new Texture and store it in the memory
+	Texture* createNewTextureInMemory(std::string textureName, unsigned int width, unsigned int height, GLint format, bool mipmap);
+	//Delete the texture stored in memory
+	void deleteTextureFromMemory(std::string textureName);
+	//Return the texture stored in memory
+	Texture* getTextureInMemory(std::string textureName);
+
+	//Load a mesh into the memory
+	Mesh* loadMeshIntoMemoryFromDisk(std::string meshName);
+	//Delete a mesh from memory
+	void deleteMeshFromMemory(std::string meshName);
+	//Return the mesh stored in meory
+	Mesh* getMeshInMemory(std::string meshName);
+
+private:
+	//Map containing all the shaders loaded
+	std::unordered_map<std::string, Shader*> _shaderMap;
+	//Map containing all the texture loaded
+	std::unordered_map<std::string, Texture*> _textureMap;
+	//Map containing all the meshes loaded
+	std::unordered_map<std::string, Mesh*> _meshMap;
 };
 
