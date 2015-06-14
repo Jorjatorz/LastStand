33d7b9fedfb83ddce9aa686e543504e9b984aa72
Jorge Sanchez <jorjatorz@gmail.com>
2015-06-14 20:44:28 +0200

    Added class FResourceManager

---
+++ LastStand/FResourceManager.cpp
@@ -0,0 +1,11 @@
+#include "FResourceManager.h"
+
+
+FResourceManager::FResourceManager()
+{
+}
+
+
+FResourceManager::~FResourceManager()
+{
+}
---
+++ LastStand/FResourceManager.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//Class that manage all external resources that are added to the game.
+class FResourceManager
+{
+public:
+	FResourceManager();
+	~FResourceManager();
+};
+
